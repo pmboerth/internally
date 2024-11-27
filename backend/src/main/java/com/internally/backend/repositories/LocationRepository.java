dc261373d6fb5409bf1568c82606e57f7bc21819
Aaron Kim <44210329+aaronkim218@users.noreply.github.com>
2024-11-26 19:20:24 -0500

    Revert "location implementation (#13)" (#17)
    This reverts commit edb2cdc191a665f0f8dce1c194616ea3d13c7c46.

--- backend/src/main/java/com/internally/backend/repositories/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.internally.backend.repositories;
-
-import com.internally.backend.models.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
